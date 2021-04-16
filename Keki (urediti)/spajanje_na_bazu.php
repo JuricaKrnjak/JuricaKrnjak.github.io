4d47ccb99ed2368d4b11c5f69b682f9d7ab74aed
JuricaKrnjak <82598035+JuricaKrnjak@users.noreply.github.com>
2021-04-16 16:34:06 +0200

    Delete Keki (urediti) directory

--- Keki (urediti)/spajanje_na_bazu.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $spajanje=new mysqli("localhost", "root", "", "keks");
-    if($spajanje->connect_errno){
-        echo "Veza s bazom nije uspostavljena <br>";
-    }
-    $spajanje->set_charset("utf8");
-?>
